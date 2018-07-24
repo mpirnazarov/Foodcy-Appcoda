a9211c638108700d17bc65ed785e7844105aeee0
Muslimbek <muslimbek.pirnazarov@gmail.com>
2018-07-25 01:10:41 +0200

    Restaurant images where introduced and implemented

--- Foodcy/Controllers/RestaurantTableViewController.swift
+++ Foodcy/Controllers/RestaurantTableViewController.swift
@@ -12,6 +12,13 @@ class RestaurantTableViewController: UITableViewController {
 
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
+    var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg",
+                            "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg",
+                            "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg",
+                            "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg",
+                            "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg",
+                            "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,13 +43,14 @@ class RestaurantTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
 
         // Configure the cell...
-        cell.imageView?.image = UIImage(named: "restaurant")
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
         cell.textLabel?.text = restaurantNames[indexPath.row]
         return cell
-    }
+    }
 
     /*
     // Override to support conditional editing of the table view.
