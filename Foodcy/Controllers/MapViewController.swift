b38bff25a08c55df31a3067a6a230ec2792a0e84
Muslimbek <muslimbek.pirnazarov@gmail.com>
2018-08-01 20:27:16 +0200

    Added annotation to MapView, working!

---
+++ Foodcy/Controllers/MapViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MapViewController.swift
+//  Foodcy
+//
+//  Created by Muslimbek on 01/08/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet var mapView: MKMapView!
+    
+    // MARK: - Actions
+
+    
+    // MARK: - Variables
+    var restaurant:Restaurant!
+    
+    // MARK: - Native methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: {
+            placemarks, error in
+            if error != nil {
+                print(error!)
+                return
+            }
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Foodcy/Controllers/RestaurantDetailViewController.swift
+++ Foodcy/Controllers/RestaurantDetailViewController.swift
@@ -48,6 +48,10 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             let destinationController = segue.destination as! ReviewViewController
             destinationController.restaurant = restaurant
         }
+        else if segue.identifier == "showMap" {
+            let destinationController = segue.destination as! MapViewController
+            destinationController.restaurant = restaurant
+        }
     }
     
     @objc func showMap() {
@@ -78,6 +82,29 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         // Initializing TapGesture for identifying the map in the footer of tableView
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(showMap))
         mapView.addGestureRecognizer(tapGestureRecognizer)
+        
+        // Adding anotation to map we created
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: {
+            placemarks, error in
+            if error != nil {
+                print(error!)
+                return
+            }
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                if let location = placemark.location {
+                    // Display the annotation
+                    annotation.coordinate = location.coordinate
+                    self.mapView.addAnnotation(annotation)
+                    // Set the zoom level
+                    let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 250, 250)
+                    self.mapView.setRegion(region, animated: false)
+                }
+            } })
     }
     
     override func viewWillAppear(_ animated: Bool) {
